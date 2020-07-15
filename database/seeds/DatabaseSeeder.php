e09a2e9366fded0d04835fbf4c4ac27e95825f7e
egyramadhan <36892796+egyramadhan@users.noreply.github.com>
2020-07-15 14:53:38 +0700

    master

---
+++ database/seeds/DatabaseSeeder.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use App\User;
+use Spatie\Permission\Models\Role;
+use Spatie\Permission\Models\Permission;
+
+class DatabaseSeeder extends Seeder
+{
+    /**
+     * Seed the application's database.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $user = User::create([
+            'name' => 'admin',
+            'email' => 'z@z.com',
+            'password' => bcrypt('123456')
+        ]);
+
+        $role = Role::create(['name' => 'Admin']);
+
+        $permissions = Permission::pluck('id', 'id')->all();
+
+        $role->syncPermissions($permissions);
+
+        $user->assignRole([$role->id]);
+    }
+}
